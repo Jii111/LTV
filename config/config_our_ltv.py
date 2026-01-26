5f977432eb883de28a09aa03df511c05d6733fab
지원 <jiii111@jiwon-ui-MacBookAir.local>
2026-01-26 16:14:01 +0900

    update dataset folder and fix import error

--- config/config_our_ltv.py
+++ config/config_our_ltv.py
@@ -2,6 +2,7 @@ config = {}
 
 ### Main Configuration ###
 config['exp_name'] = 'exps/ltv'  
+config['gpus'] = ['0']
 config['models'] = ['meta-llama/Llama-3.1-8B'] # 'meta-llama/Llama-2-7b-hf', 'meta-llama/Llama-2-13b-hf', 'Qwen/Qwen3-8B', 'Qwen/Qwen2.5-7B'
 config['datasets'] = ['sst2','sst5','mr','subj','trec','hate_speech18','agnews','dbpedia'] 
 config['num_shot'] = 30 # number of shots (NOT per class)
